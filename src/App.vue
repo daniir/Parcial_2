9090bdda6388194562edbfe1de6ba2d3382763b1
daniir <danrodas03@gmail.com>
2018-11-18 22:32:46 -0500

    Llamado del inicio de sesion mensaje de bienvenida

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 align='center'>Bienvenido</h1>
+    <inicio/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import inicio from './components/formulario.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    inicio
   }
 }
 </script>
+
 
 <style>
 #app {
