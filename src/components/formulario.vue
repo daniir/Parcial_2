b7c545f13f34b2f3c4047cf562e094fd25f0cb4a
daniir <danrodas03@gmail.com>
2018-11-18 22:33:08 -0500

    cuerpo de formulario y tabla de datos

---
+++ src/components/formulario.vue
@@ -0,0 +1,127 @@
+<template>
+    <div id='formulario'>
+        <h1>Formulario de usuarios</h1>
+        <br>
+        <br>
+        <el-input style="width: 200px;height:15px" placeholder="Usuario" type="text" v-model="usuarioNmb"></el-input>
+        <br>
+        <br>
+        <el-input style="width: 200px;height:15px" placeholder="Apellido" type="text" v-model="usuarioApd"></el-input>
+        <br>
+        <br>
+        <el-input style="width: 200px;height:15px" Placeholder="Ciudad" type="text" v-model="usuarioCi"></el-input>
+        <br>
+        <br>
+        <p>Sexo</p>
+        <el-radio v-model="sexoUsuario" label="Masculino">Masculino</el-radio>
+        <el-radio v-model="sexoUsuario" label="Femenino">Femenino</el-radio>
+        <br>
+        <br>
+        <p>Fecha de Nacimiento</p>
+        <el-date-picker value-format="dd-MM-yyyy" placeholder="Fecha de nacimiento" v-model="fechaNac"></el-date-picker>
+        <br>
+        <br>
+        <p>Estado de usuario</p>
+        <el-radio v-model="estUsuario" label="Activo">Activo</el-radio>
+        <el-radio v-model="estUsuario" label="Inactivo">Inactivo</el-radio>
+        <br>
+        <br>
+        <el-input style="width: 900px;height:15px" placeholder="Dirección de entrega" type="text" v-model="usuarioDir"></el-input>
+        <br>
+        <br>
+        <el-button type="primary" @click="guardarUser">Agregar</el-button>
+        <br>
+        <br>
+        <el-table :data="tableData1.filter(buscar_usuario => !buscar || buscar_usuario.nombre.toLowerCase().includes(buscar.toLowerCase()))"
+        style="width: 100%">
+        <el-table-column fixed prop="nombre" label="Nombre" width="180">
+        </el-table-column>
+        <el-table-column prop="apellido" label="Apellido" width="180">
+        </el-table-column>
+        <el-table-column prop="ciudad" label="Ciudad" width="180">
+        </el-table-column>
+        <el-table-column prop="sexo" label="Sexo" width="180">
+        </el-table-column>
+        <el-table-column value-format="dd-MM-yyyy" prop="fechaNac" label="Fecha de nacimiento" width="180">
+        </el-table-column>
+        <el-table-column prop="estado" label="Estado" width="180">
+        </el-table-column>
+        <el-table-column prop="direccion" label="Dirección" width="180">
+        </el-table-column>
+        <el-table-column fixed="right" align="right" width="120">
+        <template slot="header" slot-scope="slot">
+        <el-input v-model="buscar" size="mini" placeholder="Buscador" />
+        </template>
+        </el-table-column>
+        </el-table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "formulario",
+  data: function() {
+    return {
+      usuarioNmb: "",
+      usuarioApd: "",
+      usuarioCi: "",
+      sexoUsuario: "",
+      fechaNac: "",
+      estUsuario: "",
+      usuarioDir: "",
+      tableData1: [
+        {
+          nombre: "Daniel",
+          apellido: "Rodas",
+          ciudad: "Panamá",
+          sexo: "Masculino",
+          fechaNac: "1-01-1111",
+          estado: "Activo",
+          direccion: "Tumba Muerto"
+        }
+      ]
+    };
+  },
+  methods: {
+    guardarUser: function() {
+      this.tableData1.push({
+        nombre: this.usuarioNmb,
+        apellido: this.usuarioApd,
+        ciudad: this.usuarioCi,
+        sexo: this.sexoUsuario,
+        fechaNac: this.fechaNac,
+        estado: this.estUsuario,
+        direccion: this.usuarioDir
+      });
+      (this.usuarioNmb = ""),
+        (this.usuarioApd = ""),
+        (this.usuarioCi = ""),
+        (this.sexoUsuario = ""),
+        (this.fechaNac = ""),
+        (this.estUsuario = ""),
+        (this.usuarioDir = "");
+      console.log(
+        this.nombre,
+        this.apellido,
+        this.ciudad,
+        this.sexo,
+        this.fechaNac,
+        this.estado,
+        this.direccion
+      );
+    }
+  },
+  buscar_usuario: function() {
+    const index = this.tableData1.findIndex(
+      item => item.nombre === this.buscar
+    );
+    console.log(index);
+    if (index > -1) {
+      console.log(this.tableData1[index]);
+      return true;
+    }
+    return false;
+  }
+};
+</script>
+
