89a9262c8c48395bc9360a695637437938190c8f
daniir <danrodas03@gmail.com>
2018-11-19 22:53:16 -0500

    llamado del componente formulario

--- src/components/inicio.vue
+++ src/components/inicio.vue
@@ -1,37 +1,49 @@
 <template>
     <div id='inicio'>
+        <center v-if="!verificar">
         <el-input style="width: 200px;height:15px" placeholder="Usuario" type="text" v-model="usuarioSs" id="nombre"></el-input>
         <br>
         <br>
         <el-input style="width: 200px;height:15px" placeholder="Contraseña" type="password" v-model="usuarioPsswrd" id="clave"></el-input>
         <br>
         <br>
-        <el-button type="primary" @click="validar">Ingresar</el-button>
+        <el-button type="primary"  @click="validar">Ingresar</el-button>
+        </center>
+        <center v-if="verificar"><formulario/></center>
     </div>
 </template>
 
 <script>
+import formulario from "./formulario.vue";
+
 export default {
-    name: 'inicio',
-    data: function(){
-        return {
-            usuarioSs:'',
-            usuarioPsswrd:'',
-        }
-    },
-    methods:{
-    validar: function () {
-      if (this.usuarioSs === 'admin' && this.usuarioPsswrd === 'admin') {
-           this.$router.replace({ name: "formulario" });
-      }
-      else {
-          alert("el usuario " + this.usuarioSs + " y/o la contraseña son erroneas");
-          document.getElementById("nombre").value = "";
-          document.getElementById("clave").value = "";
-          document.getElementById("nombre").focus();
+  name: "inicio",
+  components: {
+    formulario
+  },
+  data: function() {
+    return {
+      usuarioSs: "",
+      usuarioPsswrd: "",
+      verificar: false
+    };
+  },
+  methods: {
+    validar: function() {
+      if (this.usuarioSs == "admin" && this.usuarioPsswrd == "contra") {
+        this.verificar = true;
+      } else {
+        alert(
+          "el usuario " + this.usuarioSs + " y/o la contraseña son erroneas"
+        );
+        document.getElementById("nombre").value = "";
+        document.getElementById("clave").value = "";
+        document.getElementById("nombre").focus();
       }
     }
   }
-}
+};
 </script>
+
+
 
