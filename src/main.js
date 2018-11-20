0c9ca1fe92040a77b6c8bd4f10a03c70077a02c0
daniir <danrodas03@gmail.com>
2018-11-19 23:31:07 -0500

    parcial2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  data:function(){
+    return {
+      usuarioSs:'',
+      usuarioPsswrd:'',
+      usuarioNmb: "",
+      usuarioApd: "",
+      usuarioCi: "",
+      sexoUsuario: "",
+      fechaNac: "",
+      estUsuario: "",
+      usuarioDir: "",
+      nombre: "",
+      apellido: "",
+      ciudad: "",
+      sexo: "",
+      estado: "",
+      direccion: ""
+    }
+  }
 }).$mount('#app')
